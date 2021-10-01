4f9e120ed6e622fa2d61ec3c6c5d2bb45cad870b
TarasCoder <4kaskad@gmail.com>
2021-10-01 15:10:29 +0300

    adding logic

---
+++ app.js
@@ -0,0 +1,23 @@
+let testDate = "2021-10-05";
+function convertDate(inputDate) {
+  let year = inputDate.substring(0, 4);
+  let month = inputDate.substring(5, 7);
+  let day = inputDate.substring(8, 10);
+
+  if (month.substring(0, 1) == 0) {
+    month = month.substring(1, 2);
+  }
+  if (day.substring(0, 1) == 0) {
+    day = day.substring(1, 2);
+  }
+  let date = new Date(year, month-1, day);
+  let options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  let convertedDate = date.toLocaleDateString("en", options);
+  console.log(convertedDate);
+}
+convertDate(testDate);
+// SHOULD BE:    JUNE 13, 2021
